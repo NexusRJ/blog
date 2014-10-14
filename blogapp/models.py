dc3d063b907b594a196476cd95111f49ac3d4f7c
RJ <NexusRJ@qq.com>
2014-10-14 22:24:50 +0800

    change models for migrations and comments

--- blogapp/models.py
+++ blogapp/models.py
@@ -10,12 +10,28 @@ class Archives(models.Model):
     archivetype = models.CharField(max_length=150, default='books')
 
     class Meta:
-        ordering = ('-timestamp', 'title', 'archivetype' )
+        ordering = ('-timestamp',)
+
+
+class Comments(models.Model):
+    archiveid = models.ForeignKey(Archives)
+    archivetitle = models.TextField(max_length=150)
+    content = models.TextField(max_length=300)
+    author = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    link = models.URLField(max_length=100, blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('id',)
 
 
 class ArchivesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'timestamp', 'archivetype','id')
+    list_display = ('title', 'timestamp', 'archivetype', 'body')
 
 
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'author', 'archivetitle')
 
 admin.site.register(Archives,ArchivesAdmin)
+admin.site.register(Comments,CommentsAdmin)

2df4303cec9fdb84cbfc830f31f198753c88fbe3
RJ <NexusRJ@qq.com>
2014-10-14 22:41:25 +0800

    fuck

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.comments',
     'blogapp',
     'gunicorn',
 )
